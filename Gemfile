fd4287cc036d6daa8ce50b024fbfe5dcb0eaa2ab
Elliot Murphy <statik@users.noreply.github.com>
2019-04-04 11:00:57 -0400

    small fixes and updates
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- Gemfile
+++ Gemfile
@@ -4,3 +4,5 @@ source "https://rubygems.org"
 gem "inspec"
 gem "rbnacl", ">= 3.2", "< 5.0"
 gem "bcrypt_pbkdf", ">= 1.0", "< 2.0"
+
+gem "ed25519", "~> 1.2"
--- run-checks.sh
+++ run-checks.sh
@@ -5,7 +5,7 @@
 #set -e
 
 # uncomment to debug
-# set -x
+#set -x
 
 #echo "Pulling inspec docker container..."
 #docker pull chef/inspec
@@ -18,9 +18,10 @@ if [ "$target_server" == "" ]; then
   exit 1
 fi
 
-#user=`whoami` # chanage this line to override ssh user
-user='ubuntu'
-
+user=`whoami` # change this line to override ssh user
+#user='ubuntu'
+# docker version of this needs debugging against volume mapping overlap conflicts on MacOS
+#eval `ssh-agent`
 #inspec="docker run -it --rm -v $SSH_AUTH_SOCK:/tmp/agent.sock -e 'SSH_AUTH_SOCK=/tmp/agent.sock' chef/inspec"
 inspec=`command -v inspec 2> /dev/null`
 if [ "$inspec" == "" ]; then
